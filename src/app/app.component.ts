6f2316bd9d43620de64a52ae2f4ec07b3f03c45c
Rafael Barros <rafaelbarros.df@gmail.com>
2018-11-24 23:03:15 -0200

    Introdução ao Material Design Lite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,42 @@
-import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface Course {
+  key: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'angular7-firebase';
 
-  items: Observable<any[]>;
+  coursesCollection: AngularFirestoreCollection<Course>;
+  courses: Observable<Course[]>;
+  snapshot: any;
 
-  constructor(db: AngularFirestore) {
-    this.items = db.collection('/courses').valueChanges();
+  constructor(private afs: AngularFirestore) { }
+
+  public update(key: string) {
+    console.log(key);
+  }
+
+  ngOnInit() {
+    this.coursesCollection = this.afs.collection('courses');
+    // this.courses = this.coursesCollection.valueChanges(); // observable of notes data
+    this.snapshot = this.coursesCollection.snapshotChanges().pipe(
+        map(
+          changes => changes.map(c => ({ key: c.payload.doc.id, ...c.payload.doc.data() }))
+        )
+    );
+
+    this.courses = this.snapshot;
+
   }
 }
